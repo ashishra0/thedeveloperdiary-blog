dbc51b2dde9a020b81fd9c7a6011fb96f79ee841
Ashish Rao <ashishrao2598@gmail.com>
2017-06-10 00:51:32 +0530

    ordered by published date

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,7 +2,7 @@ class ArticlesController < ApplicationController
   before_action :set_article, only: [:show, :edit, :update, :destroy]
 
   def index
-    @articles = current_user.nil? ? Article.paginate(page: params[:page], per_page: 5) : current_user.articles.paginate(page: params[:page], per_page: 5)
+    @articles = current_user.nil? ? Article.paginate(page: params[:page], per_page: 5).order('publish_date DESC') : current_user.articles.paginate(page: params[:page], per_page: 5).order('publish_date DESC')
   end
 
   def new
